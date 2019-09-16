61cbca67eba97dc511e32776da991bed3c88b9f1
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-16 22:33:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each_pair{ |category, types|
+    types.each_pair{|type, names|
+      names.map{|name|
+        if pigeons[name]
+          if !pigeons[name][category.to_sym]
+            pigeons[name][category.to_sym] = []
+          end
+          pigeons[name][category.to_sym] << type.to_s
+        else
+          pigeons[name] = {}
+          pigeons[name][category.to_sym] = []
+          pigeons[name][category.to_sym] << type.to_s
+        end
+      }
+    }
+  }
+  p pigeons
 end
